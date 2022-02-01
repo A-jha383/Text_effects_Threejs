a9101d0685ef7a376f493bfb156b57351b6a32c5
AYUSH JHA <ayushj383@gmail.com>
2022-02-02 01:27:45 +0530

    final code

--- src/script.js
+++ src/script.js
@@ -20,7 +20,8 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
-
+const matcapTexture = textureLoader.load('/textures/matcaps/3.png')
+const matcapTexture1 = textureLoader.load('/textures/matcaps/8.png')
 // fonts
 const fontLoader = new THREE.FontLoader()
 fontLoader.load(
@@ -39,11 +40,35 @@ fontLoader.load(
                 bevelSegments:5
             }
         )
-        textGeometery.computeBoundingBox()
-        console.log(textGeometery.boundingBox)
-        const textMaterial = new THREE.MeshBasicMaterial()
+        textGeometery.center()
+        // textGeometery.computeBoundingBox()
+        // textGeometery.translate(
+        //     -(textGeometery.boundingBox.max.x-0.02)*0.5,
+        //     -(textGeometery.boundingBox.max.y-0.02)*0.5,
+        //     -(textGeometery.boundingBox.max.z-0.03)*0.5
+        // )
+        // console.log(textGeometery.boundingBox)
+        const textMaterial = new THREE.MeshMatcapMaterial({matcap:matcapTexture})
     const text = new THREE.Mesh(textGeometery,textMaterial)
     scene.add(text)
+
+    
+
+    const donutGeometry = new THREE.TorusBufferGeometry(0.3,0.2,20,45)
+    const donutMaterial = new THREE.MeshMatcapMaterial({matcap:matcapTexture1})
+   
+    for(let i=0;i<300;i++){
+        
+        const donut =new THREE.Mesh(donutGeometry,donutMaterial)
+        donut.position.x =(Math.random()-0.5)*10
+        donut.position.y =(Math.random()-0.5)*10
+        donut.position.z =(Math.random()-0.5)*10
+        donut.rotation.x = Math.random() * Math.PI
+        donut.rotation.y = Math.random() * Math.PI
+        const scale = Math.random()
+        donut.scale.set(scale,scale,scale)
+        scene.add(donut)
+    }
     }
     
 )
